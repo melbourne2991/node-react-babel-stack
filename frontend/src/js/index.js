f819e993a1ee4822ad75d431e53cf59c5f1f7335
Will <wleach1992@gmail.com>
2015-06-25 16:42:42 +1000

    fixed

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,4 +1,13 @@
 import '../sass/app.scss';
 import 'babel/polyfill';
+import React from 'react';
 
-console.log('Hello world!');
+class TestComponent extends React.Component {
+	render() {
+		return(
+			<h1>Hello World!</h1>
+		)
+	}
+}
+
+React.render(<TestComponent/>, document.querySelector('body'));
